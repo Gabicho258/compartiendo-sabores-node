62a718800b1753620cf76f2879aac1cd322aba7a
Gabriel <gmamanican@unsa.edu.pe>
2023-11-07 17:07:07 -0500

    Añadiendo rutas para usuarios
    Co-authored-by: CarloDiazP <CarloDiazP@users.noreply.github.com>
    Co-authored-by: elopezqu <elopezqu@users.noreply.github.com>

--- api/routes/user.router.js
+++ api/routes/user.router.js
@@ -1,18 +1,23 @@
 import express from "express";
 import { UserController } from "../controllers/index.js";
 
-const { createUser, getAllUsers, updateUser } = UserController;
+const { createUser, getAllUsers, updateUser, getOneUser, login } =
+  UserController;
 
 const router = express.Router();
 
 const USER_ROUTES = {
   GET_ALL: "/user",
+  GET_ONE: "/user/:id",
   CREATE: "/user/create",
   UPDATE: "/user/update/:id",
+  LOGIN: "/auth/login",
 };
 
 router.get(USER_ROUTES.GET_ALL, getAllUsers);
+router.get(USER_ROUTES.GET_ONE, getOneUser);
 router.put(USER_ROUTES.UPDATE, updateUser);
 router.post(USER_ROUTES.CREATE, createUser);
+router.post(USER_ROUTES.LOGIN, login);
 
 export default router;
