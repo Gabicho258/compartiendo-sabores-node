d53fdc8c2a88a6cc72417230d11586594d0dbe1b
Gabriel <gmamanican@unsa.edu.pe>
2023-11-07 17:00:56 -0500

    Indexando rutas, controladores y middlewares
    Co-authored-by: CarloDiazP <CarloDiazP@users.noreply.github.com>
    Co-authored-by: elopezqu <elopezqu@users.noreply.github.com>

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,4 +1,5 @@
 import * as UserController from "./user.controller.js";
 import * as CommentController from "./comment.controller.js";
+import * as RecipeController from "./recipe.controller.js";
 
-export { UserController, CommentController };
+export { UserController, CommentController, RecipeController };
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,5 @@
 import UserRouter from "./user.router.js";
 import CommentRouter from "./comment.router.js";
+import RecipeRouter from "./recipe.router.js";
 
-export { UserRouter, CommentRouter };
+export { UserRouter, CommentRouter, RecipeRouter };
