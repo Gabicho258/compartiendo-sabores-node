58cde2a979a1ce3275354cddb6a0676657b6d786
Gabriel <gmamanican@unsa.edu.pe>
2023-11-07 17:06:25 -0500

    Añadiendo rutas de comentarios
    Co-authored-by: CarloDiazP <CarloDiazP@users.noreply.github.com>
    Co-authored-by: elopezqu <elopezqu@users.noreply.github.com>

--- api/routes/comment.router.js
+++ api/routes/comment.router.js
@@ -1,14 +1,21 @@
 import express from "express";
 import { CommentController } from "../controllers/index.js";
 
-const { createComment } = CommentController;
+const { createComment, deleteComment, getAllComment, updateComment } =
+  CommentController;
 
 const router = express.Router();
 
 const COMMENT_ROUTES = {
+  GET_ALL: "/comment",
   CREATE: "/comment/create",
+  UPDATE: "/comment/update/:id",
+  DELETE: "/comment/delete/:id",
 };
 
+router.get(COMMENT_ROUTES.GET_ALL, getAllComment);
 router.post(COMMENT_ROUTES.CREATE, createComment);
+router.put(COMMENT_ROUTES.UPDATE, updateComment);
+router.delete(COMMENT_ROUTES.DELETE, deleteComment);
 
 export default router;
