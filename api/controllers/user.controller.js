d06112d8a521aedc759fb475a158760757625338
Gabriel <gmamanican@unsa.edu.pe>
2023-11-07 17:05:45 -0500

    Añadiendo controladores para usuario + login
    Co-authored-by: CarloDiazP <CarloDiazP@users.noreply.github.com>
    Co-authored-by: elopezqu <elopezqu@users.noreply.github.com>

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,6 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import { User } from "../models/index.js";
 
 export const getAllUsers = async (req, res) => {
@@ -9,9 +12,28 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
-export const createUser = async (req, res) => {
-  const newUser = new User({ ...req.body });
+export const getOneUser = async (req, res) => {
+  const { id: user_id } = req.params;
+
   try {
+    const user = await User.findById(user_id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+export const createUser = async (req, res) => {
+  // Encriptamos la contraseña
+  const { email, password } = req.body;
+  const passwordHash = await bcrypt.hash(password, 15);
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ error: "User already exists" });
+    // Creamos un nuevo usuario con la contraseña encriptada
+    const newUser = new User({ ...req.body, password: passwordHash });
     const user = await newUser.save();
     user && res.status(201).json(user);
   } catch (error) {
@@ -33,6 +55,34 @@ export const updateUser = async (req, res) => {
       res.status(204).json({ error: "User not found" });
     }
   } catch (error) {
-    res.status(500).json({ error: "qweqwe" });
+    res.status(500).json({ error: "Server error" });
   }
+};
+
+export const login = async (req, res) => {
+  // Verificamos la petición
+  const { email, password } = req.body;
+  if (!(email && password))
+    return res.status(400).json({ error: "Bad request" });
+
+  // Comprobamos que exista un usuario con el correo ingresado
+  const user = await User.findOne({ email });
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  // Validamos la contraseña encriptada
+  bcrypt.compare(password, user.password, (error, isValid) => {
+    if (isValid) {
+      jwt.sign(
+        { email: user.email },
+        process.env.SECRET_KEY,
+        (error, token) => {
+          if (!error) {
+            res.status(200).json({ token, user });
+          } else res.status(401).json({ error: "Invalid credentials" });
+        }
+      );
+    } else {
+      res.status(401).send();
+    }
+  });
 };
