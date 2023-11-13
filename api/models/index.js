5d0fbf8caa4a7a47d152f6c1376b70784a992348
Gabriel <gmamanican@unsa.edu.pe>
2023-11-12 22:29:06 -0500

    creando modelo para chat en BD

--- api/models/index.js
+++ api/models/index.js
@@ -1,5 +1,6 @@
 import User from "./user.model.js";
 import Recipe from "./recipe.model.js";
 import Comment from "./comment.model.js";
+import Chat from "./chat.model.js";
 
-export { User, Comment, Recipe };
+export { User, Comment, Recipe, Chat };
