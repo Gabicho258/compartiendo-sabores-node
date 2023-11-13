08840e56437deac9a48021be12bb8e9fdc5ba942
Gabriel <gmamanican@unsa.edu.pe>
2023-11-12 22:28:03 -0500

    creando rutas para el API de chat

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,6 @@
 import UserRouter from "./user.router.js";
 import CommentRouter from "./comment.router.js";
 import RecipeRouter from "./recipe.router.js";
+import ChatRouter from "./chat.router.js";
 
-export { UserRouter, CommentRouter, RecipeRouter };
+export { UserRouter, CommentRouter, RecipeRouter, ChatRouter };
